2aad02ac4a3ab796e720ac766171a55ed0ba3e24
farkmu45 <farukmicros@gmail.com>
2020-05-04 10:03:14 +0700

    Add layout

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,26 @@
+  <div class="horizontal-main bg-dark-transparent clearfix">
+      <div class="horizontal-mainwrapper container clearfix">
+          <div class="desktoplogo">
+              <a href="index.html"><img src="{{url('/assets/images/brand/logo1.png')}}" alt=""></a>
+          </div>
+          <div class="desktoplogo-1">
+              <a href="index.html"><img src="{{url('/assets/images/brand/logo1.png')}}" alt=""></a>
+          </div>
+          <nav class="horizontalMenu clearfix d-md-flex">
+              <ul class="horizontalMenu-list">
+                  <li aria-haspopup="true"><a href="#home">Beranda</a></li>
+                  <li aria-haspopup="true"><a href="#why">Kenapa Investasi</a></li>
+                  <li aria-haspopup="true"><a href="#invest">Investasi Online</a></li>
+                  <li aria-haspopup="true"><a href="#about">Profil</a></li>
+                  <li aria-haspopup="true"><a href="#special">Penawaran Spesial</a></li>
+                  <li aria-haspopup="true"><a href="#product">Produk</a></li>
+              </ul>
+              <ul class="mb-0">
+                  <li aria-haspopup="true" class="mt-3 d-none d-lg-block top-postbtn">
+                      <span><a class="btn btn-secondary ad-post " href="">Login | Register</a></span>
+                  </li>
+              </ul>
+          </nav>
+          <!--Nav-->
+      </div>
+  </div>
