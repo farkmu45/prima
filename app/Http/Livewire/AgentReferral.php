cb1fd018b5fe8be8224568a7ef08c07734aad2dc
farkmu45 <farukmicros@gmail.com>
2020-05-11 10:33:23 +0700

    Fix agent referral

--- app/Http/Livewire/AgentReferral.php
+++ app/Http/Livewire/AgentReferral.php
@@ -8,18 +8,9 @@ use Livewire\Component;
 class AgentReferral extends Component
 {
 
-    public function send()
-    {
-        AgentRequest::create([
-            'user_id' => auth()->user()->id
-        ]);
-    }
-
-
     public function render()
     {
-        $request = AgentRequest::where('user_id', auth()->user()->id)->count();
         $referral = auth()->user()->referral;
-        return view('livewire.agent-referral')->withRequest($request)->withReferrals($referral);
+        return view('livewire.agent-referral')->withReferrals($referral);
     }
 }
