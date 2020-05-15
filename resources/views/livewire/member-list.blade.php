15f489302111cf8a3206814ac223ffe43592c81b
farkmu45 <farukmicros@gmail.com>
2020-05-15 22:39:13 +0700

    Fix misplaced wallet

--- resources/views/livewire/member-list.blade.php
+++ resources/views/livewire/member-list.blade.php
@@ -55,7 +55,7 @@
 													<td>{{$member->role->name}}</td>
 													<td>{{$member->referral_code == null ? '' : $member->referral_code }}</td>
 													<td>{{$member->alt_referral_code == null ? '' : $member->alt_referral_code }}</td>
-													<td>Rp. {{$member->wallet ? 0 : number_format($member->wallet,0,',','.')}}</td>
+													<td>Rp. {{$member->wallet ? number_format($member->wallet,0,',','.')}} : 0</td>
 													@if (!$member->deleted_at)
 													<td>
 														<p class="badge badge-success">Active</p>
