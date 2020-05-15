e0f6faaae12d677a5f6252f81e26170501cd3f8e
farkmu45 <farukmicros@gmail.com>
2020-05-15 23:34:08 +0700

    Fix pricing route

--- app/Http/Middleware/VerifyPhone.php
+++ app/Http/Middleware/VerifyPhone.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class VerifyPhone
 {
@@ -15,6 +16,11 @@ class VerifyPhone
      */
     public function handle($request, Closure $next)
     {
+        if (Auth::guest()) {
+            return redirect('/login');
+        }
+
+
         if (!$request->user()->hasVerifiedPhone()) {
             return redirect('/verifyphone');
         }
